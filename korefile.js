909893a74143ab1ea0f8ff302518ea750f974764
luboslenco <lubos.lenco@gmail.com>
2019-05-03 08:40:22 +0200

    Apply patch

--- korefile.js
+++ korefile.js
@@ -19,34 +19,34 @@ if (platform === Platform.Windows) {
 	project.addFile('Libraries/nfd/nfd_win.cpp');
 }
 else if (platform === Platform.Linux) {
-	project.addIncludeDir("/usr/include/gtk-3.0 ");
-	project.addIncludeDir("/usr/include/at-spi2-atk/2.0");
-	project.addIncludeDir("/usr/include/at-spi-2.0");
-	project.addIncludeDir("/usr/include/dbus-1.0");
-	project.addIncludeDir("/usr/lib/x86_64-linux-gnu/dbus-1.0/include");
-	project.addIncludeDir("/usr/include/gtk-3.0");
-	project.addIncludeDir("/usr/include/gio-unix-2.0/");
-	project.addIncludeDir("/usr/include/mirclient");
-	project.addIncludeDir("/usr/include/mircore");
-	project.addIncludeDir("/usr/include/mircookie");
-	project.addIncludeDir("/usr/include/cairo");
-	project.addIncludeDir("/usr/include/pango-1.0");
-	project.addIncludeDir("/usr/include/harfbuzz");
-	project.addIncludeDir("/usr/include/pango-1.0");
-	project.addIncludeDir("/usr/include/atk-1.0");
-	project.addIncludeDir("/usr/include/cairo");
-	project.addIncludeDir("/usr/include/pixman-1");
-	project.addIncludeDir("/usr/include/freetype2");
-	project.addIncludeDir("/usr/include/libpng16");
-	project.addIncludeDir("/usr/include/gdk-pixbuf-2.0");
-	project.addIncludeDir("/usr/include/libpng16");
-	project.addIncludeDir("/usr/include/glib-2.0");
-	project.addIncludeDir("/usr/lib/x86_64-linux-gnu/glib-2.0/include");
-	project.addLib("gtk-3");
-	project.addLib("gobject-2.0");
-	project.addLib("glib-2.0");
-
-	project.addFile('Libraries/nfd/nfd_gtk.c');
+	// project.addIncludeDir("/usr/include/gtk-3.0 ");
+	// project.addIncludeDir("/usr/include/at-spi2-atk/2.0");
+	// project.addIncludeDir("/usr/include/at-spi-2.0");
+	// project.addIncludeDir("/usr/include/dbus-1.0");
+	// project.addIncludeDir("/usr/lib/x86_64-linux-gnu/dbus-1.0/include");
+	// project.addIncludeDir("/usr/include/gtk-3.0");
+	// project.addIncludeDir("/usr/include/gio-unix-2.0/");
+	// project.addIncludeDir("/usr/include/mirclient");
+	// project.addIncludeDir("/usr/include/mircore");
+	// project.addIncludeDir("/usr/include/mircookie");
+	// project.addIncludeDir("/usr/include/cairo");
+	// project.addIncludeDir("/usr/include/pango-1.0");
+	// project.addIncludeDir("/usr/include/harfbuzz");
+	// project.addIncludeDir("/usr/include/pango-1.0");
+	// project.addIncludeDir("/usr/include/atk-1.0");
+	// project.addIncludeDir("/usr/include/cairo");
+	// project.addIncludeDir("/usr/include/pixman-1");
+	// project.addIncludeDir("/usr/include/freetype2");
+	// project.addIncludeDir("/usr/include/libpng16");
+	// project.addIncludeDir("/usr/include/gdk-pixbuf-2.0");
+	// project.addIncludeDir("/usr/include/libpng16");
+	// project.addIncludeDir("/usr/include/glib-2.0");
+	// project.addIncludeDir("/usr/lib/x86_64-linux-gnu/glib-2.0/include");
+	// project.addLib("gtk-3");
+	// project.addLib("gobject-2.0");
+	// project.addLib("glib-2.0");
+	// project.addFile('Libraries/nfd/nfd_gtk.c');
+	project.addFile('Libraries/nfd/nfd_zenity.c');
 }
 else {
 	project.addFile('Libraries/nfd/nfd_cocoa.m');
